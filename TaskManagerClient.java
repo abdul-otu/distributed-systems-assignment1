db30e9551399f61c8c53052898d5c4d0322abc11
Abdul Shahid <93552245+abdul-otu@users.noreply.github.com>
2023-10-19 22:31:23 -0400

    Update TaskManagerClient.java

--- TaskManagerClient.java
+++ TaskManagerClient.java
@@ -8,35 +8,42 @@ import java.net.Socket;
 public class TaskManagerClient {
     public static void main(String[] args) {
 
+        // Set default font for all Swing components
         Font defaultFont = new Font("Arial", Font.PLAIN, 16);
         UIManager.put("Button.font", defaultFont);
         UIManager.put("Label.font", defaultFont);
         UIManager.put("TextField.font", defaultFont);
         UIManager.put("TextArea.font", defaultFont);
 
+        // Create the GUI
         JFrame frame = new JFrame("Task Manager Client");
         JTextArea textArea = new JTextArea(20, 30);
         textArea.setEditable(false);
         frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
 
+        // Create the create button GUI component
         JButton createButton = new JButton("Create Task");
         frame.add(createButton, BorderLayout.NORTH);
         
-
+        // Add action listeners to the create button
         createButton.addActionListener(new ActionListener() {
             @Override
+            // When the create button is clicked, this method is called
             public void actionPerformed(ActionEvent e) {
                 while (true) {
+
+                    // Ask the user for the task title
                     String title = JOptionPane.showInputDialog("Enter task title:");
                     if (title == null) {
                         // User canceled, exit the loop
                         break;
                     } else if (title.trim().isEmpty()) {
-                        // Task name is empty, show an error message and ask again
+                        // User entered an empty string, show an error message and continue the loop
                         JOptionPane.showMessageDialog(null, "Task name cannot be empty.");
                         continue;
                     }
 
+                    // Ask the user for the task date and time
                     JPanel dateInputPanel = new JPanel();
                     JTextField yearField = new JTextField(3);
                     JTextField monthField = new JTextField(3);
@@ -44,6 +51,7 @@ public class TaskManagerClient {
                     JTextField hourField = new JTextField(3);
                     JTextField minuteField = new JTextField(3);
 
+                    // Add the text fields to the panel
                     dateInputPanel.add(new JLabel("YY:"));
                     dateInputPanel.add(yearField);
                     dateInputPanel.add(new JLabel("MM:"));
@@ -55,23 +63,27 @@ public class TaskManagerClient {
                     dateInputPanel.add(new JLabel("MM:"));
                     dateInputPanel.add(minuteField);
 
+                    // Show the panel in a dialog box
                     int result = JOptionPane.showConfirmDialog(null, dateInputPanel, "Enter Date", JOptionPane.OK_CANCEL_OPTION);
                     if (result != JOptionPane.OK_OPTION) {
                         // User canceled, exit the loop
                         break;
                     }
 
+                    // Get the text from the text fields
                     String yearText = yearField.getText();
                     String monthText = monthField.getText();
                     String dayText = dayField.getText();
                     String hourText = hourField.getText();
                     String minuteText = minuteField.getText();
 
+                    // Check if any of the text fields are empty
                     if (yearText.isEmpty() || monthText.isEmpty() || dayText.isEmpty() || hourText.isEmpty() || minuteText.isEmpty()) {
                         JOptionPane.showMessageDialog(null, "Please fill in all date fields.");
                         continue;
                     }
 
+                    // Check if the text fields contain numeric values
                     try {
                         int year = Integer.parseInt(yearText);
                         int month = Integer.parseInt(monthText);
@@ -79,34 +91,39 @@ public class TaskManagerClient {
                         int hour = Integer.parseInt(hourText);
                         int minute = Integer.parseInt(minuteText);
 
+                        // Check if the date and time values are valid (month 1-12, day 1-31, hour 0-23, minute 0-59)
                         if (month < 1 || month > 12 || day < 1 || day > 31 || hour < 0 || hour > 23 || minute < 0 || minute > 59) {
                             JOptionPane.showMessageDialog(null, "Invalid date and time values. Please check your input.");
                             continue;
                         }
 
+                        // Create the task string
                         String task = title + " " + year + "-" + month + "-" + day + " " + hour + ":" + minute;
 
-                        // Check for duplicates
+                        // Check if the task already exists in the task list
                         if (taskAlreadyExists(textArea, task)) {
+
+                            // Ask the user if they want to create a duplicate task
                             int choice = JOptionPane.showConfirmDialog(null, "This task already exists. Do you want to create a duplicate task?", "Task Already Exists", JOptionPane.YES_NO_OPTION);
                             if (choice != JOptionPane.YES_OPTION) {
-                                continue; // User chose not to create a duplicate
+                                // User chose not to create a duplicate task, continue the loop
+                                continue;
                             }
                         }
 
-                        try (Socket socket = new Socket("localhost", 12345);
+                        try (Socket socket = new Socket("localhost", 12000);                        
+                            // Create a BufferedReader to read the server's response and a PrintWriter to send the task
                              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                              PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
+                            // Send the task to the server
                             out.println("create");
                             out.println(task);
                             refreshTaskList(textArea);
-
                         } catch (IOException ex) {
                             ex.printStackTrace();
                         }
-
-                        break; // Successfully created the task, exit the loop
+                        break;
                     } catch (NumberFormatException ex) {
                         JOptionPane.showMessageDialog(null, "Please enter numeric values for date and time fields.");
                     }
@@ -114,24 +131,33 @@ public class TaskManagerClient {
             }
         });
 
+        // Create the delete button GUI component
         JButton deleteButton = new JButton("Delete Task");
         frame.add(deleteButton, BorderLayout.SOUTH);
 
+        // Add action listeners to the delete button
         deleteButton.addActionListener(new ActionListener() {
             @Override
+            // When the delete button is clicked, this method is called
             public void actionPerformed(ActionEvent e) {
-                try (Socket socket = new Socket("localhost", 12345);
+                try (Socket socket = new Socket("localhost", 12000);
+
+                    // Create a BufferedReader to read the server's response and a PrintWriter to send the task
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                      PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
         
+                    // Ask the user for the index of the task to delete
                     String indexStr = JOptionPane.showInputDialog("Enter the index of the task to delete:");
                     try {
+                        // Convert the index string to an integer
                         int indexToDelete = Integer.parseInt(indexStr);
 
+                        // Send the index to the server
                         if (indexToDelete > 0) {
                             out.println("delete");
                             out.println(indexToDelete);
         
+                            // Display the new task list
                             refreshTaskList(textArea);
                         }
                     } catch (NumberFormatException ex) {
@@ -144,31 +170,39 @@ public class TaskManagerClient {
         });
         
 
+        // Set up the frame for the main window and display it
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
+        // Display the initial task list
         refreshTaskList(textArea);
     }
 
+    // This method refreshes the task list by sending a "view" command to the server
     private static void refreshTaskList(JTextArea textArea) {
-        try (Socket socket = new Socket("localhost", 12345);
+        try (Socket socket = new Socket("localhost", 12000);
+
+            // Create a BufferedReader to read the server's response and a PrintWriter to send the task
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
+            // Send the "view" command to the server
             out.println("view");
             String task;
-            textArea.setText(""); // Clear the text area
 
-            int taskIndex = 1; // Initialize task index
+            // Clear the text area
+            textArea.setText("");
+            int taskIndex = 1;
 
+            // Read the tasks from the server and display them in the text area
             while ((task = in.readLine()) != null) {
                 if (task.isEmpty()) {
                     break;
                 }
 
-                // Display tasks without "Tasks:"
+                // Skip the "Tasks:" line
                 if (!task.equals("Tasks:")) {
                     textArea.append(taskIndex + ". " + task + "\n");
                     taskIndex++;
@@ -180,6 +214,7 @@ public class TaskManagerClient {
         }
     }
 
+    // This method checks if a task already exists in the task list
     private static boolean taskAlreadyExists(JTextArea textArea, String taskToCheck) {
         String text = textArea.getText();
         return text.contains(taskToCheck);
